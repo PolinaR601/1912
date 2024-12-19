ac72019ecdc863832e07d567414de305f35d54fa
tihon <presetova5@gmail.com>
2024-12-20 00:53:47 +0300

    2

--- Reshetova_task5/src/main/java/ru/reshetova/reshetova_task5/GoController.java
+++ Reshetova_task5/src/main/java/ru/reshetova/reshetova_task5/GoController.java
@@ -24,10 +24,10 @@ public class GoController {
     void GoButtonOnAction(ActionEvent event) {
         double x=Double.parseDouble(xTextField.getText().toString());
         double y=Double.parseDouble(yTextField.getText().toString());
-        if(Math.pow(x,2)+Math.pow(y,2)==Math.pow(25,2) && y==-Math.abs(x) || Math.pow(x,2)+Math.pow(y,2)==Math.pow(25,2)&& y==Math.abs(x)||y==-Math.abs(x)&&y==Math.abs(x)){
+        if(Math.pow(x,2)+Math.pow(y,2) < Math.pow(25,2) && y < -Math.abs(x) && y < Math.abs(x)){
+            result.setText("да");
+        }else if ((Math.pow(x,2)+Math.pow(y,2) == Math.pow(25,2) || y == -Math.abs(x) || y == Math.abs(x))&& y<25&& x>-25 && x<25){
             result.setText("На границе");
-        }else if (Math.pow(x,2)+Math.pow(y,2) < Math.pow(25,2) && y < -Math.abs(x) && y < Math.abs(x)){
-            result.setText("Да");
         } else
             result.setText("Нет");
     }
